eecd38ef726b696cc8d1af6bd65584ef9d6b5973
luiz.quelves@gmail.com <lq872913>
2017-05-14 18:55:23 -0400

    Generate HMAC

---
+++ lib/security.rb
@@ -0,0 +1,20 @@
+require 'openssl'
+require "base64"
+require 'digest'
+
+class Security
+  @key = '2T02j&xwE#tQA#e'
+  def self.doHashSHA1(authorization)
+    data = 'GET590baa77d6b4ec0004902cbf'
+    digest = OpenSSL::Digest.new('sha1')
+
+    hmac = OpenSSL::HMAC.digest(digest, @key, authorization)
+    return Base64.encode64(hmac)
+  end
+end
+#Security.doHashSHA1('GET590baa77d6b4ec0004902cbf')
+
+
+
+
+
--- lib/tasks/stock.rake
+++ lib/tasks/stock.rake
@@ -1,7 +1,11 @@
+
+
 namespace :stock do
+  require 'security'
   desc "Check Stock level"
   task check: :environment do
-    puts "#{Time.now} - Deliver success!"
+    puts "#{Time.now} - Generate Authorization!"
+    puts Security.doHashSHA1('GET590baa77d6b4ec0004902cbf')
   end
 
 end
