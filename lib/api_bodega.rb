1760f56d54de13743c2130f7adb4d45ca90a4fa1
luiz.quelves <luiz.quelves@gmail.com>
2017-05-17 03:13:18 -0400

    Puts results of warehouse

--- lib/api_bodega.rb
+++ lib/api_bodega.rb
@@ -148,7 +148,7 @@ class APIBodega
   def self.producir_Stock_Sin_Pago(sku, cantidad)
     hmac = doHashSHA1('PUT'+sku.to_s + cantidad.to_s)
     params = {'sku' => sku, 'cantidad' => cantidad}
-    result put_url(@PRODUCIR_STOCK_SIN_PAGO, params, hmac)
+    result = put_url(@PRODUCIR_STOCK_SIN_PAGO, params, hmac)
     puts result
     return result
   end
