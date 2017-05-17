1194e735cb91df94397d07f26ab2318afc750e91
Sebastian Piza <spiza@uc.cl>
2017-05-16 21:00:46 -0400

    mergiando dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,39 +1,54 @@
+require 'api_bodega.rb'
 class DashboardsController < ActionController::Base
+  @ARREGLO_SKUS = ['4', '6', '19', '20', '23', '26', '27', '38', '42', '53']
+  ##ACEITE MARA - CREMA - SEMOLA - CACAO - HARINA - SAL -LEVADURA - SEMILLAS_MARAVILLA - CEREAL_MAIZ - PAN INT
+  @ARREGLO_ALMACENES = ['590baa77d6b4ec0004902cbf', '590baa77d6b4ec0004902ea4', '590baa77d6b4ec0004902cbd', '590baa77d6b4ec0004902cbe', '590baa77d6b4ec0004902ea5']
+  ##GEN-GEN2-RECEP-DESPA-PULM
+
   def index
-    @dicc = create_prod_dict
-    @dicc_almacenes = create_alm_dict
+    @ARREGLO_SKUS = ['4', '6', '19', '20', '23', '26', '27', '38', '42', '53']
+    @ARREGLO_ALMACENES = ['590baa77d6b4ec0004902cbf', '590baa77d6b4ec0004902ea4',
+       '590baa77d6b4ec0004902cbd', '590baa77d6b4ec0004902cbe',
+        '590baa77d6b4ec0004902ea5']
+    @dicc_skus = {}
+    obtain_skus(@ARREGLO_ALMACENES, @dicc_skus)
+    @lista_almacenes = []
+    obtener_almacenes(@lista_almacenes)
+
   end
 
-  def create_prod_dict
-    products = Product.all
-    dicc = {}
-    products.each do |prod|
-      if dicc[prod.sku] == nil
-        dicc[prod.sku] = 1
-      else
-        dicc[prod.sku] += 1
-      end
-    end
-    return dicc
-  end
-
-  def create_alm_dict
-    dicc_almacenes = {}
-    almacenes = Warehouse.all
-    almacenes.each do |alma|
-      if dicc_almacenes[alma.warehouse_id] == nil #No se encuentra en el dicc!
-        if alma.reception
-          dicc_almacenes[alma.warehouse_id] = ['Recepcion', alma.spaceUsed, alma.spaceTotal]
-        elsif alma.lung
-          dicc_almacenes[alma.warehouse_id] = ['Pulmon', alma.spaceUsed, alma.spaceTotal]
-        elsif alma.dispatch
-          dicc_almacenes[alma.warehouse_id] = ['Despacho', alma.spaceUsed, alma.spaceTotal]
+  def obtain_skus(lista_alma, diccionario)
+    lista_alma.each do |alma|
+      response = APIBodega.get_skusWithStock(alma)
+      response.each do |ris|
+        if diccionario[ris["_id"]] == nil
+          diccionario[ris["_id"]] = ris["total"]
         else
-          dicc_almacenes[alma.warehouse_id] = ['General', alma.spaceUsed, alma.spaceTotal]
+          diccionario[ris["_id"]] += ris["total"]
         end
       end
     end
-    return dicc_almacenes
+  end
+
+  def obtener_almacenes(lista_almacenes)
+    almanaque = APIBodega.get_almacenes()
+    for i in 0..almanaque.length - 1 
+      lista = []
+      str = ""
+      if almanaque[i]["pulmon"]
+        str = "Pulmon"
+      elsif almanaque[i]["despacho"]
+        str = "Despacho"
+      elsif almanaque[i]["recepcion"]
+        str = "Recepcion"
+      else
+        str = "General"
+      end
+      lista << str
+      lista << almanaque[i]["usedSpace"]
+      lista << almanaque[i]["totalSpace"]
+      lista_almacenes << lista
+    end
   end
 
 end
