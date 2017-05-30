4cd06eec3a80814d48a495ca5fd780e8246837ea
quelves <luiz.quelves@gmail.com>
2017-05-30 01:20:36 -0400

    smodel and control for iic3103pay

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528032276) do
+ActiveRecord::Schema.define(version: 20170530035849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,15 @@ ActiveRecord::Schema.define(version: 20170528032276) do
     t.string   "id_factura"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+  end
+
+  create_table "payproxies", force: :cascade do |t|
+    t.decimal  "amount"
+    t.integer  "boleta_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "state"
+    t.index ["boleta_id"], name: "index_payproxies_on_boleta_id", unique: true, using: :btree
   end
 
   create_table "products", force: :cascade do |t|
