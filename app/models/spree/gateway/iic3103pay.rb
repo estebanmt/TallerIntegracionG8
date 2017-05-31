5c4ff1354c73b037d628fbad284bb095020b93c5
quelves <luiz.quelves@gmail.com>
2017-05-31 09:19:45 -0400

    call to pay

--- app/models/spree/gateway/iic3103pay.rb
+++ app/models/spree/gateway/iic3103pay.rb
@@ -1,12 +1,18 @@
+require 'uri'
+
 class Spree::Gateway::IIC3103pay  < Spree::Gateway
+
+
+
+  @API_URL_PAGO = ENV["URL_API_PAGO"]
+  @URL_PAY_PROXY = ENV["URL_PAY_PROXY"]
+  @PARAM_URL_OK = 'callbackUrl='
+  @PARAM_URL_FAIL = 'cancelUrl='
+  @PARAM_BOLETA_ID = 'boletaId'
+
   def provider_class
     Spree::Gateway::IIC3103pay
   end
-
-  #def payment_source_class
-  #  Spree::CreditCard
-  #end
-
 
   def source_required?
     false
@@ -16,12 +22,19 @@ class Spree::Gateway::IIC3103pay  < Spree::Gateway
     false
   end
 
-
   def method_type
     'iic3103pay'
   end
 
+
+
   def purchase(amount, transaction_details, options = {})
+    @boleta_id = 1
+
+
+    @url = @API_URL_PAGO + @PARAM_URL_OK + URI.encode(@URL_PAY_PROXY + '/' + @boleta_id + '/sucess') + '&' + URI.encode(@PARAM_URL_FAIL +  @URL_PAY_PROXY + '/fail')  + '&' + @PARAM_BOLETA_ID + @boleta_id
+    puts "iic3103 - url_pago  #{@url}"
+
     puts "iic3103 - purchase #{amount}"
     ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
   end
---
+++ lib/api_pago.rb
@@ -0,0 +1,7 @@
+class ApiPago
+
+
+
+
+
+end
