a83707c3bcb644f20aec7980c8e10e01adf02fcb
quelves <luiz.quelves@gmail.com>
2017-07-04 20:30:31 -0400

    spree api chagnes

--- lib/api_spree.rb
+++ lib/api_spree.rb
@@ -10,8 +10,12 @@ class APISpree
   @API_URL_SPREE = ENV["API_URL_SPREE"]
 
 
+  @SPREE_API_KEY = "a685e0757f4d0175ce1a17a924a330bfdcf8a939216171f0"
+  @API_URL_SPREE = "http://localhost:4000/api/v1/"
+
+
   def initialize()
-    create_all_object
+
   end
 
   @products = [ [4, 'Aceite de Maravilla', 'Producto procesado', 8, 'Lts', 412, 200, 1, 2, 1.205],
@@ -25,9 +29,10 @@ class APISpree
                 [42, 'Cereal Maiz', 'Producto procesado', 8, 'Kg', 812, 200, 3, 0, 2.743],
                 [53, 'Pan Integral', 'Producto', 8, 'Kg', 934, 620, 5, 0, 2.400]]
 
-  def self.create_all_object()
+  def self.create_all_object
+
     @products.each do |product|
-      create_sku(product[1], product[2], product[3], product[6])
+      create_product(product[0].to_s, product[1].to_s, product[2].to_s, product[5].to_i)
     end
   end
 
@@ -38,13 +43,39 @@ class APISpree
 
   def self.post_url(uri, params)
     @url = @API_URL_SPREE + uri
-    begin
-      @response=RestClient.post @url, params.to_json, :content_type => :json, :accept => :json, :X-Spree-Token => @SPREE_API_KEY
-      puts "response code " + @response.code.to_s
-    rescue
-      puts "--------------------INTENTANDO NUEVAMENTE--------------------------"
-      retry
+    body_params = query_params(params)
+    puts @url
+    puts body_params
+
+    @response = RestClient::Request.new({
+                                           method: :post,
+                                           url: @url,
+      user: 'someone',
+      password: 'mybirthday',
+      payload: params.to_json,
+      headers: { :accept => :json, content_type: :json, :'X-Spree-Token' => @SPREE_API_KEY }
+    }).execute do |response, request, result|
+      case response.code
+      when 400
+        [ :error, parse_json(response.to_str) ]
+      when 200
+        [ :success, parse_json(response.to_str) ]
+      else
+        fail "Invalid response #{response.to_str} received."
+      end
     end
+
+
+
+
+#
+# begin
+      #@response=RestClient.post @url, params.to_json, :content_type => :json, :accept => :json, :'X-Spree-Token' => @SPREE_API_KEY
+      puts "response code " + @response.code.to_s
+#    rescue Exception => e
+#      puts e.message
+#      puts e.backtrace.inspect
+#    end
     json = JSON.parse(@response.body)
     puts json
   end
@@ -55,7 +86,7 @@ class APISpree
     @url = @API_URL_SPREE + uri
     puts @url
 
-    @response=RestClient.put @url, params.to_json, :content_type => :json, :accept => :json, :X-Spree-Token => @SPREE_API_KEY
+    @response=RestClient.put @url, params.to_json, :content_type => :json, :accept => :json, :"X-Spree-Token" => @SPREE_API_KEY
     json = JSON.parse(@response.body)
     return json
   end
@@ -64,8 +95,8 @@ class APISpree
     if params != nil
       queryParams = "";
       params.each do |field, value|
-        queryParams.concat(field).concat("=").concat(value).concat("&");
-        #queryParams = queryParams + field + "=" + value + "&";
+        #queryParams.concat(field).concat("=").concat(value).concat("&");
+        queryParams = queryParams + field.to_s + "=" + value.to_s + "&";
       end
       return queryParams
     else
@@ -75,3 +106,4 @@ class APISpree
 
 end
 
+APISpree.create_all_object
